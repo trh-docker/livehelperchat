6b87ea40d9c4fab661ec640d3ece9ebec740d9d6
spivegin <spivegin@txtsme.com>
2021-06-22 10:45:53 -0400

    changed for caddy v1

--- files/bash/entry.sh
+++ files/bash/entry.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/dumb-init /bin/sh
 # gobetween from-file <path> [flags]
 /usr/sbin/php-fpm7.1 --nodaemonize --fpm-config /etc/php/7.1/fpm/php-fpm.conf &
-caddy run -config /opt/caddy/Caddyfile &
+# caddy run -config /opt/caddy/Caddyfile &
+caddy -conf /opt/caddy/Caddyfile &
 gobetween from-file /opt/caddy/livechat.json -f json
