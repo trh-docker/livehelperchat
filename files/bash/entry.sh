bb8a721be0f2444b290a53ce098f8c7a1f71395d
spivegin <spivegin@txtsme.com>
2021-06-22 22:58:40 -0400

    changing php listen to tcp

--- files/bash/entry.sh
+++ files/bash/entry.sh
@@ -3,6 +3,6 @@
 /usr/sbin/php-fpm7.1 --nodaemonize --fpm-config /etc/php/7.1/fpm/php-fpm.conf &
 # caddy run -config /opt/caddy/Caddyfile &
 caddy -conf /opt/caddy/Caddyfile &
-livechatcors server &
+# livechatcors server &
 # socat -d tcp4-connect:127.0.0.1:9000 unix-connect:/run/php/php7.1-fpm.sock &
 gobetween from-file /opt/caddy/livechat.json -f json
